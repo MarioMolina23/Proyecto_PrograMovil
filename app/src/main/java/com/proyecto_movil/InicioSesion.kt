3bab54a620cc7da75b3bdc7ce916123cacf0ce83
MarioMolina23 <94089210+MarioMolina23@users.noreply.github.com>
2022-11-28 17:25:28 -0600

    Página de inicio de sesión (Falta depurarla)

---
+++ app/src/main/java/com/proyecto_movil/InicioSesion.kt
@@ -0,0 +1,11 @@
+package com.proyecto_movil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InicioSesion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio_sesion)
+    }
+}
--- app/src/main/java/com/proyecto_movil/MainActivity.kt
+++ app/src/main/java/com/proyecto_movil/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.proyecto_movil
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import org.imaginativeworld.whynotimagecarousel.CarouselItem
 import org.imaginativeworld.whynotimagecarousel.ImageCarousel
 
@@ -12,6 +14,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initEvents()
+
         /*Se llaman las imagenes desde la Web para cargarlas en el home de la app*/
         val carousel: ImageCarousel = findViewById(R.id.tv_titulo)
         list.add(CarouselItem("https://media.istockphoto.com/id/1144226219/es/vector/la-gente-que-espera-el-transporte-y-el-uso-de-internet-tel%C3%A9fono-inteligente-concepto-de.jpg?s=612x612&w=0&k=20&c=UyZTMsTcpnPDQa0g-WANx_vFrkujRbOFo0pBCCK8kuI="))
@@ -19,5 +23,14 @@ class MainActivity : AppCompatActivity() {
         list.add(CarouselItem("https://i.pinimg.com/originals/8b/f9/8d/8bf98d94773b9002b1f7e68f29e0d716.jpg"))
         /*Se crea la lista y se carga a continuación*/
         carousel.addData(list)
+    }
+
+    //Evento para abrir la pantalla de inicio de Sesion
+    fun initEvents(){
+        val button = findViewById<Button>(R.id.bt_inicio)
+        button.setOnClickListener{
+            val intent = Intent (this,InicioSesion::class.java)
+            startActivity(intent)
+        }
     }
 }
