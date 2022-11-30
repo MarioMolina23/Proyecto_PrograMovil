73a259ffea61af92ab4f8610f9c8f51c702d6f43
MarioMolina23 <94089210+MarioMolina23@users.noreply.github.com>
2022-11-30 00:37:32 -0600

    Autenticación (Inicio Sesión, Registrarse, Cerrar Sesión)

--- app/src/main/java/com/proyecto_movil/InicioSesion.kt
+++ app/src/main/java/com/proyecto_movil/InicioSesion.kt
@@ -3,14 +3,65 @@ package com.proyecto_movil
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.proyecto_movil.databinding.ActivityInicioSesionBinding
 
 class InicioSesion : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityInicioSesionBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_inicio_sesion)
+        binding = ActivityInicioSesionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        FirebaseApp.initializeApp(this)
+        auth = Firebase.auth
+
+        //Login
+        binding.btIniciarSesion.setOnClickListener{
+            hacerlogin();
+        }
 
         bt_registrase();
+    }
+
+    private fun hacerlogin() {
+        val email = binding.inicioCorreo.text.toString()
+        val clave = binding.inicioContrasena.text.toString()
+
+        auth.signInWithEmailAndPassword(email,clave)
+            .addOnCompleteListener (this) { task ->
+                if (task.isSuccessful) {
+                    Log.d("Autenticando", "Autenticado")
+                    val user = auth.currentUser
+                    actualiza(user)
+                }else {
+                    Log.d("Error autenticado", "Error")
+                    Toast.makeText(baseContext,"Falló", Toast.LENGTH_LONG).show()
+                    actualiza(null)
+                }
+            }
+    }
+
+    private fun actualiza(user: FirebaseUser?) {
+        if (user != null) {
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
+    }
+
+    public override fun onStart () {
+        super.onStart()
+        val usuario = auth.currentUser
+        actualiza(usuario)
     }
 
     fun bt_registrase(){
--- app/src/main/java/com/proyecto_movil/Registrarse.kt
+++ app/src/main/java/com/proyecto_movil/Registrarse.kt
@@ -3,15 +3,66 @@ package com.proyecto_movil
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.proyecto_movil.databinding.ActivityRegistrarseBinding
 
 class Registrarse : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityRegistrarseBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registrarse)
+        binding = ActivityRegistrarseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        FirebaseApp.initializeApp(this)
+        auth = Firebase.auth
+
+        //Login
+        binding.btRegister.setOnClickListener{
+            hacerregister();
+        }
 
         bt_cancelar();
     }
+
+    private fun hacerregister() {
+        val email = binding.registerCorreo.text.toString()
+        val clave = binding.registerContrasena.text.toString()
+
+        auth.createUserWithEmailAndPassword(email,clave)
+            .addOnCompleteListener (this) { task ->
+                if (task.isSuccessful) {
+                    Log.d("Creando usuario", "Registrado")
+                    val user = auth.currentUser
+                    actualiza(user)
+                }else {
+                    Log.d("Error creando usuario", "Error")
+                    Toast.makeText(baseContext,"Falló",Toast.LENGTH_LONG).show()
+                    actualiza(null)
+                }
+            }
+    }
+
+    private fun actualiza(user: FirebaseUser?) {
+        if (user != null) {
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
+    }
+
+    public override fun onStart () {
+        super.onStart()
+        val usuario = auth.currentUser
+        actualiza(usuario)
+    }
+
 
     fun bt_cancelar(){
         val button = findViewById<Button>(R.id.bt_cancelar)
