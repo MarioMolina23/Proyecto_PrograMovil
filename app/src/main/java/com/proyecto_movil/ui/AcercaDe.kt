ebdac294d21405c85b82a9fdd0fe8ea31b07159e
MarioMolina23 <94089210+MarioMolina23@users.noreply.github.com>
2022-12-12 14:02:11 -0600

    Agregue boton de internacionalizacion en el menu y funcionalidad de pantalla "Escribanos"

--- app/src/main/java/com/proyecto_movil/ui/AcercaDe.kt
+++ app/src/main/java/com/proyecto_movil/ui/AcercaDe.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.ImageButton
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.proyecto_movil.R
@@ -13,13 +15,36 @@ class AcercaDe : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_acerca_de)
+
+        botones(); //Se llama a la función de los botones
     }
+
+    private fun botones() {
+        val btn_terminos: ImageButton = findViewById(R.id.bt_terminos)
+        btn_terminos.setOnClickListener{
+            val intent: Intent = Intent(this, Terminos::class.java)
+            startActivity(intent)
+        }
+
+        val btn_historia: ImageButton = findViewById(R.id.bt_historia)
+        btn_historia.setOnClickListener{
+            val intent: Intent = Intent(this, Historia::class.java)
+            startActivity(intent)
+        }
+
+        val btn_equipo: ImageButton = findViewById(R.id.bt_equipo)
+        btn_equipo.setOnClickListener{
+            val intent: Intent = Intent(this, Equipo::class.java)
+            startActivity(intent)
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.home, menu)
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean { //Funcionabilidad de botones dentro del menú
         return when (item.itemId) {
             R.id.menu_home -> {
                 startActivity(Intent(this, Main_Home::class.java))
@@ -43,18 +68,6 @@ class AcercaDe : AppCompatActivity() {
             }
             R.id.menu_terminos_uso -> {
                 startActivity(Intent(this, Terminos::class.java))
-                true
-            }
-            R.id.bt_terminos -> {
-                startActivity(Intent(this, Terminos::class.java))
-                true
-            }
-            R.id.bt_historia -> {
-                startActivity(Intent(this, Historia::class.java))
-                true
-            }
-            R.id.bt_equipo -> {
-                startActivity(Intent(this, Equipo::class.java))
                 true
             }
             R.id.cerrar_session -> {
--- app/src/main/java/com/proyecto_movil/ui/Escribanos.kt
+++ app/src/main/java/com/proyecto_movil/ui/Escribanos.kt
@@ -1,23 +1,100 @@
 package com.proyecto_movil.ui
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.proyecto_movil.R
+import com.proyecto_movil.databinding.ActivityEscribanosBinding
 
 class Escribanos : AppCompatActivity() {
+
+    /*private var _binding: ActivityEscribanosBinding? = null
+    private val binding get() = _binding!!*/ //No permite acceder al Binding, revisar por que
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_escribanos)
+
+        botones(); //Se llama a la función de los botones
     }
+
+    private fun botones() {
+        val btn_escribanos: Button = findViewById(R.id.bt_escribanos)
+        btn_escribanos.setOnClickListener{
+            val intent: Intent = Intent(this, Contacto::class.java)
+            startActivity(intent)
+        }
+
+        val btn_instagram: ImageButton = findViewById(R.id.bt_instagram)
+        btn_instagram.setOnClickListener{
+            val openURL = Intent(android.content.Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("https://www.instagram.com/")
+            startActivity(openURL)
+        }
+
+        val btn_facebook: ImageButton = findViewById(R.id.bt_facebook)
+        btn_facebook.setOnClickListener{
+            val openURL = Intent(android.content.Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("https://www.facebook.com/")
+            startActivity(openURL)
+        }
+
+        val btn_web: ImageButton = findViewById(R.id.bt_web)
+        btn_web.setOnClickListener{
+            val openURL = Intent(android.content.Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("https://www.ufidelitas.ac.cr/")
+            startActivity(openURL)
+        }
+
+        //val telefono = binding.tvLlamada.text.toString()
+        val btn_llamada: ImageButton = findViewById(R.id.bt_llamada)
+        btn_llamada.setOnClickListener{
+            if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                val miIntent = Intent(
+                    Intent.ACTION_CALL,
+                    //Uri.parse("tel:$telefono"))
+                    Uri.parse("tel:+506 2206 8600"))
+                startActivity(miIntent)
+            }else{
+                Toast.makeText(this,
+                    "No hay permisos para esta llamada",
+                    Toast.LENGTH_LONG).show()
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.CALL_PHONE),
+                    123
+                )
+            }
+        }
+
+        val btn_whatsApp: ImageButton = findViewById(R.id.bt_whatsapp)
+        btn_whatsApp.setOnClickListener{
+            val intent = Intent(Intent.ACTION_VIEW)
+            val uri = "whatsapp://send?phone=50686068600&text="+
+                    getString(R.string.msg_mensaje)
+            intent.setPackage("com.whatsapp")
+            intent.data = Uri.parse(uri)
+            startActivity(intent)
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.home, menu)
         return true
     }
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
@@ -51,9 +128,5 @@ class Escribanos : AppCompatActivity() {
                 true
             } else -> super.onOptionsItemSelected(item)
         }
-        /*R.id.bt_escribanos -> {
-            startActivity(Intent(this, Contacto::class.java))
-            true
-        }*/
     }
 }
